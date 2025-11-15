22ef354830ad67cb77934358ccad9061415c0649
Franky020 <fjgarcia200@outlook.com>
2025-11-15 07:21:37 -0600

    firebase

--- fixflow_backend/fixflow_backend/settings.py
+++ fixflow_backend/fixflow_backend/settings.py
@@ -5,7 +5,7 @@ import json
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-2ruvr#e@b2&s(9c=a124rd6_9z3a4j4mxo#y7yx_dt_=zo=@&s')
-# FIREBASE_CONFIG = json.loads(os.environ.get("FIREBASE_CONFIG"))
+FIREBASE_CONFIG = json.loads(os.environ.get("FIREBASE_CONFIG"))
 
 DEBUG = True
 
