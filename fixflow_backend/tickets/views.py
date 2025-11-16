d82f7ec2008df29c98280b72e364bdb0ae0a10c1
Franky020 <fjgarcia200@outlook.com>
2025-11-16 16:38:20 -0600

    endpoint tickets

--- fixflow_backend/tickets/views.py
+++ fixflow_backend/tickets/views.py
@@ -10,6 +10,7 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import status
 from django.db.models import Count
+from rest_framework.permissions import IsAuthenticated
 
 class TicketViewSet(viewsets.ModelViewSet):
     queryset = Ticket.objects.all()
