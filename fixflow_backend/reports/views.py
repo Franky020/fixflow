2fcb6a920b66aa86722dab535874821cb1e8e49e
Franky020 <fjgarcia200@outlook.com>
2025-11-17 14:20:18 -0600

    endpoint tickets

--- fixflow_backend/reports/views.py
+++ fixflow_backend/reports/views.py
@@ -27,6 +27,23 @@ class ReportViewSet(viewsets.ModelViewSet):
 
         return Response(ReportMessageSerializer(message).data, status=201)
 
+    @action(detail=False, methods=['get'], url_path="reportes-by-user")
+    def reportes_by_user(self, request):
+        user = request.user  # Usuario logeado
+    
+        # Primero obtenemos los tickets del usuario
+        tickets_del_usuario = Ticket.objects.filter(user=user)
+    
+        # Luego buscamos los reportes ligados a esos tickets
+        reportes = Reporte.objects.filter(ticket__in=tickets_del_usuario)
+    
+        # Serializamos
+        from .serializers import ReporteSerializer  # Ajusta si usas otro serializer
+        serializer = ReporteSerializer(reportes, many=True)
+    
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
 class ReportMessageViewSet(viewsets.ModelViewSet):
     queryset = ReportMessage.objects.all()
     serializer_class = ReportMessageSerializer
