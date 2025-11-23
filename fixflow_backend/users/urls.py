8f0ab1035bf9064bdeb55742486d917313c152bd
Franky020 <fjgarcia200@outlook.com>
2025-11-23 13:12:04 -0600

    reglas en usuarios

--- fixflow_backend/users/urls.py
+++ fixflow_backend/users/urls.py
@@ -4,7 +4,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 from .serializers import Login 
 
 router = DefaultRouter()
-router.register(r'users', UserViewSet, basename='user')
+router.register(r'users', UserViewSet, basename='users')
 router.register(r'login', TokenObtainPairView.as_view(serializer_class=Login), basename='login')
 
 
