5c4e9140612f33ec6b5b47708a0a8389d22487b6
Franky020 <fjgarcia200@outlook.com>
2025-11-30 17:36:55 -0600

    Configuracion de reportes

--- fixflow_backend/companies/serializers.py
+++ fixflow_backend/companies/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Company  # Ajusta si tu modelo se llama distinto
 
 class CompanySerializer(serializers.ModelSerializer):
+    logo = serializers.ImageField(required=False)
     class Meta:
         model = Company
         fields = '__all__'  # Incluye todos los campos del modelo
