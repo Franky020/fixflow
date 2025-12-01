2d292bddfc8a80dcbbd82c53a221bc388edaf666
Franky020 <fjgarcia200@outlook.com>
2025-12-01 12:38:09 -0600

    Configuracion de reportes

--- fixflow_backend/reports/serializers.py
+++ fixflow_backend/reports/serializers.py
@@ -3,8 +3,9 @@ from .models import Report, ReportMessage  # Ajusta si tu modelo se llama distin
 
 class ReportMessageSerializer(serializers.ModelSerializer):
     class Meta:
+        image = serializers.ImageField(required=False)
         model = ReportMessage
-        fields = ['id', 'message', 'image', 'created_at']
+        fields = ['id', 'message', 'created_at']
         read_only_fields = ['created_at']  # Opcional
 
 class ReportSerializer(serializers.ModelSerializer):
